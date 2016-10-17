8d05e7de14451c5d81d6a02712b2d448afc693ee
jeanmarielepioufle <jm@nexmodeling.com>
2016-10-17 16:46:35 +0200

    made public

---
+++ R/dddTempLZ.stateX.R
@@ -0,0 +1,16 @@
+#' Temperature for each elevation zone
+#'
+#' The function \code{stateX()} process the temperature for each elevation zone
+#' @param v temperature value (scalar)
+#' @param modelTempLZ
+#'  list(nbLevelZone=a,Tlr=b,hfelt=c,midmett=d)
+#' @keywords TempLZ
+#' @export
+#' @examples
+#' \dontrun{
+#' dddTempLZ.stateX()
+#' }
+stateX <- function(v,modelTempLZ) {
+  res <- v*(1+modelTempLZ$Tlr*((modelTempLZ$hfelt-modelTempLZ$midmett)/100))
+  return(res)
+}
