f75deb38b108d0170f56586ff756719659e15d84
jeanmarielepioufle <jm@nexmodeling.com>
2016-11-01 09:23:35 +0100

    uodate function name

--- R/stateX.tempLZ.R
+++ R/stateX.tempLZ.R
@@ -1,6 +1,6 @@
 #' Temperature for each elevation zone
 #'
-#' The function \code{stateX()} process the temperature for each elevation zone
+#' The function \code{stateX.tempLZ()} process the temperature for each elevation zone
 #' @param v temperature value (scalar)
 #' @param modelTempLZ
 #'  list(nbLevelZone,Tlr,hfelt,midmett)
@@ -10,7 +10,7 @@
 #' \dontrun{
 #' stateX()
 #' }
-stateX <- function(v,modelTempLZ) {
+stateX.tempLZ <- function(v,modelTempLZ) {
   res <- v*(1+modelTempLZ$Tlr*((modelTempLZ$hfelt-modelTempLZ$midmett)/100))
   return(res)
 }
